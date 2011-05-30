ce0d03904a236d0230c83dfb2b9a4fe92c87c775
Ryan Dalzell <ryan@tullyroan.com>
2011-05-30 16:46:47 +0100

    Allow keyboard generated signals for apps using TERMIOS

--- dlterm.cpp
+++ dlterm.cpp
@@ -16,7 +16,6 @@ dlterm::dlterm()
     new_settings = initial_settings;
     new_settings.c_lflag &= ~ICANON;
     new_settings.c_lflag &= ~ECHO;
-    new_settings.c_lflag &= ~ISIG;
     new_settings.c_cc[VMIN] = 1;
     new_settings.c_cc[VTIME] = 0;
     tcsetattr(0, TCSANOW, &new_settings);
