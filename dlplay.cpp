aef3d1a98fed99f5d8626f3eaa8e04db03114c98
Ryan Dalzell <ryan@tullyroan.com>
2026-09-23 21:58:21 +0100

    Fix "no audio audio" and "no video video" message

--- dlplay.cpp
+++ dlplay.cpp
@@ -824,7 +824,7 @@ int main(int argc, char *argv[])
 
         /* in audio only mode there is no video format filter to describe the container */
         dlformat *fmt = vid_fmt? vid_fmt : aud_fmt;
-        dlmessage("info: found %s video and %s audio in %s from %s source", video? video->description() : "no video", audio? audio->description() : "no audio", fmt? fmt->description() : "an unknown container", source->description());
+        dlmessage("info: found %s video and %s audio in %s from %s source", video? video->description() : "no", audio? audio->description() : "no", fmt? fmt->description() : "an unknown container", source->description());
 
         if (video && verbose>=1)
             dlmessage("info: video format is %dx%d%c%.2f %s", pic_width, pic_height, interlaced? 'i' : 'p', framerate, pixelformatname[pixelformat]);
